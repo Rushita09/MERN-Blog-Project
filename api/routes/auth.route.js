16075bf6530a036fb30d4cf7c90129beea89875d
Rushita Arvadiya <21010101169@darshan.ac.in>
2024-02-18 13:19:58 +0530

    create signin api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,9 @@
 import express from  'express';
 import { signup } from '../controllers/auth.contoller.js';
-
+import { signin } from '../controllers/auth.contoller.js';
 const router = express.Router();
 
 
 router.post('/signup',signup);
-
+router.post('/signin',signin);
 export  default router;
