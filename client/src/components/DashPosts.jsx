01e0683fbdbfd13bdd2f40b7b0d6769a9052a596
Rushita Arvadiya <21010101169@darshan.ac.in>
2024-03-07 18:25:47 +0530

    add show more functionality to the posts results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -15,6 +15,9 @@ export default function DashPosts() {
                 const data = await res.json()
                 if(res.ok){
                     setUserPosts(data.posts);
+                    if (data.posts.length < 5) {
+                        setShowMore(false);
+                    }
                 }
             } catch (error) {
                 console.log(error.message);
@@ -23,7 +26,23 @@ export default function DashPosts() {
         if(currentUser.isAdmin){
             fetchPosts();
         }
-    }, [currentUser._id])
+    }, [currentUser._id]);
+
+    const handleShowMore = async () => {
+        const  startIndex = userPosts.length;
+        try {
+            const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+            const data=await res.json();
+            if (res.ok) {
+                setUserPosts((prev) => [...prev, ...data.posts]);
+                if (data.posts.length < 5) {
+                    setShowMore(false);
+                }
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
 
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar 
@@ -73,6 +92,13 @@ export default function DashPosts() {
                 </Table.Body>
             ))}
         </Table>
+        {
+            showMore && (
+                <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+                    Show more 
+                </button>
+            )
+        }
         </>
       ):(
         <p>You have no posts yet.</p>
