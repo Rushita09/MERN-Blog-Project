fe8dadb410db03f96ea9ebb89160e316c0004fc8
Rushita Arvadiya <21010101169@darshan.ac.in>
2024-03-22 10:53:19 +0530

    complete about and projects

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About FILE
+    <div className="min-h-screen flex items-center  justify-center">
+    <div className='max-w-2xl mx-auto p-3 text-center'>
+      <div className="text-3xl font-semibold text-center my-7">
+        <h1>About Rushita' Blog</h1>
+        <div className="text-md text-gray-500 flex flex-col gap-6 ">
+          <p>
+              Change is the only constant in life, a universal truth that touches every aspect of our existence. It presents us with opportunities for growth, renewal, and transformation, yet often accompanies a sense of uncertainty and discomfort. Whether it's a career shift, a relationship transition, or a personal evolution, navigating change requires resilience and adaptability. Embracing change means embracing the unknown, stepping out of our comfort zones, and trusting in our ability to thrive amidst life's ever-shifting landscape.
+          </p>
+          <p>
+              Each transition, no matter how daunting, offers a chance for self-discovery and personal development. It's an invitation to shed old habits, beliefs, and limitations, and embrace the possibilities that lie ahead. By cultivating a mindset of curiosity and openness, we can navigate transitions with grace and confidence. Rather than resisting change, we can choose to see it as a natural part of the human experience—a journey that leads us closer to our true selves and the life we envision.
+          </p>
+          <p>
+              Embracing change is not about avoiding discomfort or challenges but about embracing them as integral parts of our growth journey. It's about surrendering to the ebb and flow of life, knowing that each transition is an opportunity for us to evolve and expand. By embracing change with courage and resilience, we can unlock our full potential and create a life that is rich in meaning, purpose, and fulfillment.
+          </p>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import CallToAction from '../components/CallToAction'
 
 export default function Projects() {
   return (
-    <div>
-      Projects FILE
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center
+    items-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>
+        Projects
+      </h1>
+      <p className='text-md text-gray-500'>
+        Here are some of my projects. I am always open to collaborating on other projects, so feel free to reach out if you have a project
+      </p>
+      <CallToAction />
     </div>
   )
 }
